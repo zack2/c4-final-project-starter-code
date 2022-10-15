f67ca9aad142effd389b50a8df587c4b183f8648
Olivier Loukombo <olivierloukombo@gmail.com>
2022-10-15 13:28:31 +0100

    set "minLength": 1

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,16 +1,17 @@
 import {CustomAuthorizerEvent, CustomAuthorizerResult} from 'aws-lambda'
 import 'source-map-support/register'
 
-import {verify, decode} from 'jsonwebtoken'
+import {verify/*, decode*/} from 'jsonwebtoken'
 import {createLogger} from '../../utils/logger'
-import Axios from 'axios'
-import {Jwt} from '../../auth/Jwt'
+// import Axios from 'axios'
+// import {Jwt} from '../../auth/Jwt'
 import {JwtPayload} from '../../auth/JwtPayload'
-
+import request from 'request-promise';
 const logger = createLogger('auth')
 
 
 const jwksUrl = 'https://dev-kko1f-vc.us.auth0.com/.well-known/jwks.json'
+
 
 export const handler = async (
     event: CustomAuthorizerEvent
@@ -34,7 +35,7 @@ export const handler = async (
             }
         }
     } catch (e) {
-        logger.error('User not authorized', {error: e.message})
+        logger.error('User not authorized', { error: e.message })
 
         return {
             principalId: 'user',
@@ -53,22 +54,23 @@ export const handler = async (
 }
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
-    try {
-        const token = getToken(authHeader)
-        const jwt: Jwt = decode(token, {complete: true}) as Jwt
+    const token = getToken(authHeader);
 
-        // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-        // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-        let key = await getSigningKey(jwksUrl, jwt.header.kid)
+    const jwksRequest = await request({
+        uri: jwksUrl,
+        strictSsl: true,
+        json: true
+    }).promise();
 
-        return verify(
-            token,           // Token from an HTTP header to validate
-            key.publicKey,            // A certificate copied from Auth0 website
-            {algorithms: ['RS256']} // We need to specify that we use the RS256 algorithm
-        ) as JwtPayload
-    } catch (err) {
-        logger.error('Fail to authenticate', err)
-    }
+    const jwks = jwksRequest.keys;
+
+    const signingKeys = jwks.map(key => {
+        return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) };
+    });
+    const signingKey = signingKeys[0].publicKey;
+
+    return verify(token, signingKey, {algorithms: ['RS256']}) as JwtPayload;
+
 }
 
 function getToken(authHeader: string): string {
@@ -78,42 +80,12 @@ function getToken(authHeader: string): string {
         throw new Error('Invalid authentication header')
 
     const split = authHeader.split(' ')
-    const token = split[1]
+    const token = split[1];
 
     return token
 }
 
-const getSigningKey = async (jwkurl: string, kid: string) => {
-    let res = await Axios.get(jwkurl, {
-        headers: {
-            'Content-Type': 'application/json',
-            "Access-Control-Allow-Origin": "*",
-            'Access-Control-Allow-Credentials': true,
-        },
-    });
-
-    let keys = res.data.keys;
-
-    // since the keys is an array its possible to have many keys in case of cycling.
-    const signingKeys = keys.filter(key => key.use === 'sig' // JWK property `use` determines the JWK is for signing
-        && key.kty === 'RSA' // We are only supporting RSA
-        && key.kid           // The `kid` must be present to be useful for later
-        && key.x5c && key.x5c.length // Has useful public keys (we aren't using n or e)
-    ).map(key => {
-        return {kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0])};
-    });
-
-    const signingKey = signingKeys.find(key => key.kid === kid);
-    if (!signingKey) {
-        logger.error("No signing keys found")
-        throw new Error('Invalid signing keys')
-    }
-
-    logger.info("Signing keys created successfully ", signingKey)
-    return signingKey;
-};
-
-function certToPEM(cert: string) {
+function certToPEM(cert) {
     cert = cert.match(/.{1,64}/g).join('\n');
     cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
     return cert;
