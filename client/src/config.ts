0d1a5dd8147584f836d1481d3f71716b54dc8fcc
Olivier Loukombo <olivierloukombo@gmail.com>
2022-10-15 11:29:06 +0100

    add apiId

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-const apiId = 'https://ozukwz5cq0.execute-api.us-east-1.amazonaws.com/dev/todos'
+const apiId = 'ozukwz5cq0'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
